9effb5409754103a59c08fab431970bc2741f331
Akihide Nano <an74abc@gmail.com>
2018-08-26 09:52:31 +0900

    added

--- src/ore-util.lisp
+++ src/ore-util.lisp
@@ -3,3 +3,9 @@
   (:nicknames :util)
   (:use :cl))
 (in-package :ore-util)
+
+(defun time-to-string ()
+  (reduce #'(lambda (a b)
+	      (concatenate 'string a (write-to-string b)))
+	  (cdddr (reverse (multiple-value-list (get-decoded-time))))
+	  :initial-value ""))
