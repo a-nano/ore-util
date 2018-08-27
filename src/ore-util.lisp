98442ae4d20218da6099c24b9abb551d51d9b41a
Akihide Nano <an74abc@gmail.com>
2018-08-27 19:16:34 +0900

    added cformat

--- src/ore-util.lisp
+++ src/ore-util.lisp
@@ -9,3 +9,16 @@
 	      (concatenate 'string a (write-to-string b)))
 	  (cdddr (reverse (multiple-value-list (get-decoded-time))))
 	  :initial-value ""))
+
+;; (cformat t "Hello ~A~%" (if t (string 'world)) => Hello WORLD
+;; (cformat t "Hello ~A~%" (if nil (string 'world))) => nil
+
+(defmacro cformat (stream str param)
+  (let ((gparam (gensym)))
+    `(let ((,gparam ,param))
+       (format ,stream ,(concatenate 'string
+				     "~:[~;"
+				     str
+				     "~]")
+	       ,gparam
+	       ,gparam))))
